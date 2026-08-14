20e5ed264fc0c8e9d02c091cedcc06ea32f2072c
linzx0821-sudo <linzx0821@gmail.com>
2026-08-14 12:05:45 +0800

    style: sort artifact migration imports

--- migrations/versions/20260814_0002_task_artifacts.py
+++ migrations/versions/20260814_0002_task_artifacts.py
@@ -6,8 +6,8 @@ Create Date: 2026-08-14
 """
 from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 revision: str = "20260814_0002"
 down_revision: str | Sequence[str] | None = "20260814_0001"
